337954669e8b3107d631c72844036dcb9948eea3
nurulhikmah <nurulhikmah@air-nurul>
2021-07-29 21:36:20 +0700

    project pbo final exam

---
+++ src/toko/Toko.java
@@ -0,0 +1,20 @@
+package toko;
+
+import toko.view.login;
+
+/**
+ *
+ * @Rakhma
+ */
+public class Toko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       login log = new login();
+       log.setVisible(true);
+    }
+    
+}
---
+++ src/toko/view/JRXm1Loader.java
@@ -0,0 +1,16 @@
+package toko.view;
+
+import java.io.File;
+import net.sf.jasperreports.engine.design.JasperDesign;
+
+/**
+ *
+ * @author Rakhma
+ */
+class JRXm1Loader {
+
+    static JasperDesign load(File report) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
